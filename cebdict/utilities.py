9afb563f49425e1002324e909d6027eb90db6291
NoodleSushi <ytmikesstuffs@gmail.com>
2024-05-16 20:05:42 +0800

    Refactor file path handling in utilities.py

--- cebdict/utilities.py
+++ cebdict/utilities.py
@@ -1,3 +1,4 @@
+import sys
 import os
 
 '''
@@ -6,8 +7,12 @@ Default is per_line and mode is 'w'
 '''
 def write_file(name=None, contents=[], per_line=True, mode="w", add_newline=True, no_encode=False, append_newline=False):
     if name:
+        cwd = os.getcwd()
+        if '/src' in cwd:
+            cwd = cwd.replace('/src', '')
         
-        name = os.path.dirname(os.path.abspath(__file__)) + '/' + name
+        name = cwd + '/' + name
+        sys.path.insert(0, name)
 
         f = open(name, mode)
         string = ""
@@ -37,9 +42,13 @@ Can specify start and end of contents in reading a file
 '''
 def read_file(name=None, start=None, end=None, strip=False, dict_format=False, decode=False):
     if name:
-
-        name = os.path.dirname(os.path.abspath(__file__)) + '/' + name
+        cwd = os.getcwd()
+        if '/src' in cwd:
+            cwd = cwd.replace('/src', '')
         
+        name = cwd + '/' + name
+        sys.path.insert(0, name)
+
         f = open(name, "r")
         contents = []
         dictionary = {}
