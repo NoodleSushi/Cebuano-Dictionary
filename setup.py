237505294cf26c25c7a9af52358c3c0024bb58d3
NoodleSushi <ytmikesstuffs@gmail.com>
2024-05-17 10:52:32 +0800

    fasjklf

--- setup.py
+++ setup.py
@@ -12,13 +12,5 @@ setup(
   download_url = 'https://github.com/ajrequina/Cebuano-Dictionary/archive/1.0.tar.gz',
   keywords = ['pos-tag dictionary', 'cebuano-dictionary'],
   classifiers = [],
-  data_files=[
-  	('data', ['cebdict/data/cebposdict.txt']),
-  	('data/function_words', [
-  		'cebdict/data/function_words/CONJ.txt', 
-  		'cebdict/data/function_words/DET.txt',
-  		'cebdict/data/function_words/PART.txt',
-  		'cebdict/data/function_words/PRON.txt'
-  	])
-  ]
+  include_package_data=True,
 )
